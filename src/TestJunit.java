6f2ce3859e3257bbe84463a2d2039b042749c105
Chan <31901126+chansbtran@users.noreply.github.com>
2017-09-13 22:18:14 -0400

    Add files via upload

--- src/TestJunit.java
+++ src/TestJunit.java
@@ -13,10 +13,31 @@ public class TestJunit {
 	   
 	   toolTest.setToolCode("1234");
 	   assertEquals("1234", toolTest.getToolCode());
+	   toolTest.setBrand("Brand1");
+	   assertEquals("Brand1", toolTest.getBrand());
+	   toolTest.setToolType("TestType");
+	   assertEquals("TestType", toolTest.getToolType());
    }
    
    @Test
-   public void testRental()
+   public void testRentalParameters()
+   {
+	   RentalParameters rental = new RentalParameters("9/3/15", 5, 50);
+	   
+	   assertEquals(50, rental.getDiscount());
+	   assertEquals(5, rental.getRentDays());
+	   assertEquals("9/3/15", rental.getRentDate());
+	   
+	   rental.setDiscount(100);
+	   assertEquals(100, rental.getDiscount());
+	   rental.setRentDate("99/99/99");
+	   assertEquals("99/99/99", rental.getRentDate());
+	   rental.setRentDays(99);
+	   assertEquals(99, rental.getRentDays());
+   }
+   
+   @Test
+   public void testRentalCalculations()
    {
 	   Tool tool1 = new Tool("Jackhammer", "Ridgid", "JAKR", 2.99);
 	   RentalParameters rental1 = new RentalParameters("9/3/15", 5, 50);
